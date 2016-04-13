38fbd4d4ecfd5f8e0a4c48de30a7f2791695326f
Rafael Garcia <rafael.garcia@clever.com>
2016-04-13 03:27:54 +0000

    lint kayvee.ts

--- lib/kayvee.ts
+++ lib/kayvee.ts
@@ -1,40 +1,34 @@
-"use strict";
-
-var _ = require('underscore');
-
-var toKeyVal = (data) => _.map(data, (v, k) => `${k}=${JSON.stringify(v)}`);
+var _ = require("underscore");
 
 // Converts a map to a string space-delimited key=val pairs
-var format = function(data) {
+function format(data) {
   return JSON.stringify(data);
-};
+}
 
 // Similar to format, but takes additional reserved params to promote logging best-practices
-var formatLog = function(source, level, title, data) {
-  // consistently output empty string for unset keys, because null values differ by language
-  if ([null, undefined].indexOf(source) >= 0) { source = ""; }
-  if ([null, undefined].indexOf(level) >= 0) { level = ""; }
-  if ([null, undefined].indexOf(title) >= 0) { title = ""; }
-
-  var reserved = {source, level, title};
-  if (!_.isObject(data)) { data = {}; }
+function formatLog(source = "", level = "", title = "", data = {}) {
+  let reallyData = data;
+  if (!_.isObject(data)) {
+    reallyData = {};
+  }
+  const reserved = {source, level, title};
 
   // reserved keys overwrite other keys in data
-  return format(_.extend(data, reserved));
+  return format(_.extend(reallyData, reserved));
+}
+
+module.exports = {
+  format,
+  formatLog,
 };
 
-module.exports =
-  {format: format,
-  formatLog: formatLog
-  };
-
-var LOG_LEVELS =
-  {UNKNOWN: "unknown",
+const LOG_LEVELS = {
+  UNKNOWN: "unknown",
   CRITICAL: "critical",
   ERROR: "error",
   WARNING: "warning",
   INFO: "info",
-  TRACE: "trace"
-  };
+  TRACE: "trace",
+};
 
 _.extend(module.exports, LOG_LEVELS);
