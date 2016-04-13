775f7853d950d37e1137934ffa5ef78d7cb4e50e
Rafael Garcia <rafael.garcia@clever.com>
2016-04-13 03:32:04 +0000

    lint test/kayvee.js

--- test/kayvee.ts
+++ test/kayvee.ts
@@ -1,28 +1,27 @@
 var kv  = require("../lib/kayvee");
-var assert = require('assert');
-var _ = require('underscore');
-_.mixin = require('underscore.deep');
-var fs = require('fs');
+var assert = require("assert");
+var _ = require("underscore");
+_.mixin = require("underscore.deep");
+var fs = require("fs");
 
-describe('kayvee', function() {
-
-  var tests = JSON.parse(fs.readFileSync("test/tests.json"));
-  describe('.format', function() {
-    return _.each(tests['format'], function(spec) {
-      return it(spec.title, function() {
-        var actual = kv.format(spec.input.data);
-        var expected = spec.output;
-        return assert.deepEqual(JSON.parse(actual), JSON.parse(expected));
+describe("kayvee", () => {
+  const tests = JSON.parse(fs.readFileSync("test/tests.json"));
+  describe(".format", () => {
+    _.each(tests.format, (spec) => {
+      it(spec.title, () => {
+        const actual = kv.format(spec.input.data);
+        const expected = spec.output;
+        assert.deepEqual(JSON.parse(actual), JSON.parse(expected));
       });
     });
   });
 
-  return describe('.formatLog', function() {
-    return _.each(tests['formatLog'], function(spec) {
-      return it(spec.title, function() {
-        var actual = kv.formatLog(spec.input.source, spec.input.level, spec.input.title, spec.input.data);
-        var expected = spec.output;
-        return assert.deepEqual(JSON.parse(actual), JSON.parse(expected));
+  describe(".formatLog", () => {
+    _.each(tests.formatLog, (spec) => {
+      it(spec.title, () => {
+        const actual = kv.formatLog(spec.input.source, spec.input.level, spec.input.title, spec.input.data);
+        const expected = spec.output;
+        assert.deepEqual(JSON.parse(actual), JSON.parse(expected));
       });
     });
   });
